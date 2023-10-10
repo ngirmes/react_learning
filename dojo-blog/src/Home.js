12e354ae21883340f2624b689e68fb061398c564
Nicholas Girmes <n.girmes@csu.fullerton.edu>
2023-10-09 21:01:45 -0700

    Minor Updates

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,5 +1,7 @@
 // When you need a reactive value (something that might change at some point)
-// we use the useState hook to do that
+// we use the useState hook to do that, hooks always start with 'use'
+
+//Props are a way to pass data from a parent component to a child component
 
 import {useState} from 'react';
 import BlogList from './BlogList';
@@ -13,9 +15,14 @@ const Home = () => {
 
     ]);
 
+
+const handleDelete = (id) => {
+    const newBlogs = blogs.filter()
+}
+
     return( // blogs={blogs} is an example of a prop
         <div className='home'>
-            <BlogList blogs={blogs} title="All blogs!"/>
+            <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete}/>
             <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs!"/>
         </div>
     );
@@ -26,6 +33,7 @@ export default Home;
 /* The below code exemplifies the use of reactive values and state changing
 If used, any and all other code must be commented out
 const Home = () => {
+// event object can be useful as it has many properties, this is shown down below with the 'e'
 
     // let name = 'doodle';
     const [name, setName] = useState('doodle');
