8fb6438ffa4373da5b197b9026f3c74cee1dd93a
Nicholas Girmes <n.girmes@csu.fullerton.edu>
2023-10-12 17:18:07 -0700

    Changes

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,27 +1,36 @@
-import { useState, useEffect } from 'react';
-import BlogList from './BlogList';
+import { useState, useEffect } from 'react'
+import BlogList from './BlogList'
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [name, setName] = useState('mario');
+    const [blogs, setBlogs] = useState(null)
+    const [isPending, setIsPending] = useState(true)
+    const [name, setName] = useState('mario')
+    const[error, setError] = useState(null)
 
+    // To retrieve data and catch network and data retrieval errors
     useEffect(() => {
-        setTimeout( () => {
-            fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
+        fetch('http://localhost:8000/blogs') //Returns a promise
+            .then(response => {
+                if(!response.ok){
+                    throw Error('Error: could not fetch data for the resource') //Throws error if the response is not ok (!200)
+                }
+                return response.json(); //Parses the json into a javascript object and returns another promise
             })
             .then(data => {
-                setBlogs(data);
-                setIsPending(false);
+                setBlogs(data); //Setting blog variable with retrieved data
+                setIsPending(false) //Removes loading message
+                setError(null) //Set to null to avoid unnecessary error message
             })
-        }, 1000)
-    }, []);
+            .catch(err => {
+                setIsPending(false) //Removes loading message
+                setError(err.message) //Sets error message
+            })
+    }, [])
 
     return( // blogs={blogs} is an example of a prop
         <div className='home'>
+            {error && <div>{ error }</div>}
             { isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={ blogs } title="All blogs!"/>}
             {/*<BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs!"*/}
@@ -30,4 +39,4 @@ const Home = () => {
     );
 }
 
-export default Home;
+export default Home
